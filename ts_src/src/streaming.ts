233c6275fee99ece629079004ed4317dfcade7b7
Tam Pham <tampham47@live.com>
2022-10-27 16:45:05 +0700

    feat: add typescript project

---
+++ ts_src/src/streaming.ts
@@ -0,0 +1,56 @@
+// @ts-ignore
+import signalr from 'signalr-client';
+
+const addSlash = (str: string) => {
+  return str.substr(-1) !== '/' ? str + '/' : str;
+};
+
+const api = {
+  SIGNALR: 'signalr',
+};
+
+const resoleURL = (baseURL: string, query: string) => {
+  return addSlash(baseURL) + query;
+};
+
+class Streaming {
+  _client: any;
+  connected: () => void;
+  reconnecting: () => void;
+
+  constructor(options: any) {
+    const url = resoleURL(options.url, api.SIGNALR);
+    this._client = new signalr.client(url, ['FcMarketDataV2Hub'], 10, true);
+
+    this.connected = () => null;
+    this.reconnecting = () => null;
+
+    this._client._eventsListener = [];
+    this._client.headers['Authorization'] = options.token;
+
+    this._client.serviceHandlers.connected = () => {
+      this.connected();
+    };
+    this._client.serviceHandlers.reconnecting = () => {
+      this.reconnecting();
+    };
+  }
+
+  start = () => {
+    this._client.start();
+  };
+
+  getClient = () => {
+    return this._client;
+  };
+
+  subscribe = (
+    channel: string,
+    event: string,
+    callback: (message: string) => void,
+  ) => {
+    this._client.on(channel, event, callback);
+  };
+}
+
+export default Streaming;
