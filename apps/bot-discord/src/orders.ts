ccaad996d0b771bb4fcb653bb6869109f21c87b9
Tam Pham <tampham47@live.com>
2025-03-14 09:57:04 +0700

    chore: using binance

--- apps/bot-discord/src/orders.ts
+++ apps/bot-discord/src/orders.ts
@@ -1,4 +1,4 @@
-const { UMFutures } = require('@binance/futures-connector');
+import { PortfolioClient } from 'binance';
 
 const BINANCE_API_KEY = process.env.BINANCE_API_KEY || '';
 const BINANCE_API_SECRET = process.env.BINANCE_API_SECRET || '';
@@ -6,41 +6,14 @@ const BINANCE_API_SECRET = process.env.BINANCE_API_SECRET || '';
 console.log('BINANCE_API_KEY', BINANCE_API_KEY);
 console.log('BINANCE_API_SECRET', BINANCE_API_SECRET);
 
-const umFuturesClient = new UMFutures(BINANCE_API_KEY, BINANCE_API_SECRET, {
-  baseURL: 'https://fapi.binance.com',
+const pmClient = new PortfolioClient({
+  api_key: BINANCE_API_KEY,
+  api_secret: BINANCE_API_SECRET,
 });
 
 export const placeOrder = async (): Promise<number> => {
   try {
-    const batchOrders = [
-      {
-        symbol: 'BNBUSDT',
-        side: 'BUY',
-        type: 'LIMIT',
-        quantity: '0.1',
-        price: '400',
-        timeInForce: 'GTC',
-      },
-      {
-        symbol: 'BNBUSDT',
-        side: 'SELL',
-        type: 'LIMIT',
-        quantity: '0.1',
-        price: '8000',
-        timeInForce: 'GTC',
-      },
-    ];
-
-    const positionMode = await umFuturesClient.getPositionMode();
-    console.log('Position Mode:', positionMode.data);
-
-    // const response = await umFuturesClient.newOrder('BNBUSDT', 'BUY', 'LIMIT', {
-    //   quantity: '0.1',
-    //   price: '400',
-    //   timeInForce: 'GTC',
-    // });
-
-    // console.log('Market Order Response:', response.data);
+    const resp = await pmClient.testConnectivity();
     return 0;
   } catch (error) {
     console.log('Error:', error);
